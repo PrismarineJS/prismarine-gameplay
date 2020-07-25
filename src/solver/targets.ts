b66683986df080cc255d6f92865b1a15ed29ca36
TheDudeFromCI <thedudefromci@gmail.com>
2020-07-25 02:55:53 -0700

    Converted targets to use exact types.

--- src/solver/targets.ts
+++ src/solver/targets.ts
@@ -1,11 +1,12 @@
 export class Targets
 {
-  [property: string]: any;
+  blockTypes?: string[];
 
   clone(): Targets
   {
     const targets = new Targets()
-    for (const prop in this) targets[prop] = this[prop]
+
+    targets.blockTypes = this.blockTypes;
 
     return targets
   }
