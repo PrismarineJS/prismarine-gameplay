7a44323409893a736d51b4fc62b60d62a4e4a512
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-24 13:54:22 +0000

    Handled recipe solver.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/strategies/stratCraftItem.ts
+++ src/strategies/stratCraftItem.ts
@@ -1,6 +1,6 @@
 import { StrategyBase, StrategyExecutionInstance, Dependency, Callback, Solver } from '../strategy';
 import { DependencyResolver } from '../tree';
-import { MoveToInteract } from '../dependencies';
+import { MoveToInteract, ObtainItems } from '../dependencies';
 import { TaskQueue } from 'mineflayer-utils';
 import { Craft } from '../dependencies/craft';
 import { Recipe } from 'prismarine-recipe';
@@ -92,7 +92,7 @@ class CraftItemInstance extends StrategyExecutionInstance
         return ingredients.filter(x => x.count > 0).length === 0;
     }
 
-    prepareRecipe(recipes: Recipe[], cb: (err?: Error, recipe?: Recipe) => void): void
+    prepareRecipe(recipes: Recipe[], cb: (err?: Error, recipe?: Recipe) => void, resolver: DependencyResolver): void
     {
         for (const r of recipes)
         {
@@ -103,8 +103,38 @@ class CraftItemInstance extends StrategyExecutionInstance
             }
         }
 
-        // TODO Collect materials
-        cb(new Error("Not yet implemented!"))
+        // TODO Replace with "OR" task group for all recipes
+
+        const recipe = recipes[0];
+        const ingredients = this.getRequireIngredients(recipe);
+        const mcData = require('minecraft-data')(this.bot.version)
+
+        function getNextIngredient()
+        {
+            // TODO Replace with orderless task group
+            const ingredient = ingredients.pop();
+
+            if (ingredient === undefined)
+            {
+                cb(undefined, recipe);
+                return;
+            }
+            
+            resolver(new ObtainItems({
+                itemType: mcData.items[ingredient.id].name,
+                count: ingredient.count
+            }), err => {
+                if (err)
+                {
+                    cb(err);
+                    return;
+                }
+
+                getNextIngredient();
+            });
+        }
+
+        getNextIngredient();
     }
 
     handle(dependency: Dependency, resolver: DependencyResolver, cb: Callback): void
@@ -144,6 +174,6 @@ class CraftItemInstance extends StrategyExecutionInstance
             taskQueue.add((cb: Callback) => resolver(moveToCraftingTable, cb));
             taskQueue.add((cb: Callback) => this.bot.craft(recipe, craftItemTask.inputs.count, craftingTable, cb))
             taskQueue.runAll(cb);
-        });
+        }, resolver);
     }
 }
