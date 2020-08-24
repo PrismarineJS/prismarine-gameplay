4e27f99527526ada0dde4065f097c2dae49f741f
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-24 07:51:46 +0000

    Added functions for selecting recipes.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/strategies/stratCraftItem.ts
+++ src/strategies/stratCraftItem.ts
@@ -3,6 +3,7 @@ import { DependencyResolver } from '../tree';
 import { MoveToInteract } from '../dependencies';
 import { TaskQueue } from 'mineflayer-utils';
 import { Craft } from '../dependencies/craft';
+import { Recipe } from 'prismarine-recipe';
 
 export class StratCraftItem extends StrategyBase
 {
@@ -28,6 +29,27 @@ export class StratCraftItem extends StrategyBase
 
 class CraftItemInstance extends StrategyExecutionInstance
 {
+    canCraft(recipe: Recipe): boolean
+    {
+        // TODO check materials in inventory
+        return false
+    }
+
+    prepareRecipe(recipes: Recipe[], cb: (err?: Error, recipe?: Recipe) => void): void
+    {
+        for (const r of recipes)
+        {
+            if (this.canCraft(r))
+            {
+                cb(undefined, r);
+                return;
+            }
+        }
+
+        // TODO Collect materials
+        cb(new Error("Not yet implemented!"))
+    }
+
     handle(dependency: Dependency, resolver: DependencyResolver, cb: Callback): void
     {
         if (dependency.name !== 'craft')
@@ -42,21 +64,29 @@ class CraftItemInstance extends StrategyExecutionInstance
         if (recipeList.length === 0)
             throw new Error("Item is not craftable!");
 
-        const craftingTable = this.bot.findBlock({
-            matching: mcData.blocksByName.crafting_table.id,
-            maxDistance: 64
+        this.prepareRecipe(recipeList, (err, recipe) => {
+            if (err || !recipe)
+            {
+                cb(err);
+                return;
+            }
+
+            const craftingTable = this.bot.findBlock({
+                matching: mcData.blocksByName.crafting_table.id,
+                maxDistance: 64
+            });
+
+            if (!craftingTable)
+                throw new Error("Could not find crafting table!");
+
+            const moveToCraftingTable = new MoveToInteract({
+                position: craftingTable.position,
+            });
+
+            const taskQueue = new TaskQueue();
+            taskQueue.add((cb: Callback) => resolver(moveToCraftingTable, cb));
+            taskQueue.add((cb: Callback) => this.bot.craft(recipe, craftItemTask.inputs.count, craftingTable, cb))
+            taskQueue.runAll(cb);
         });
-
-        if (!craftingTable)
-            throw new Error("Could not find crafting table!");
-
-        const moveToCraftingTable = new MoveToInteract({
-            position: craftingTable.position,
-        });
-
-        const taskQueue = new TaskQueue();
-        taskQueue.add(cb => resolver(moveToCraftingTable, cb));
-        taskQueue.add(cb => this.bot.craft(recipeList[0], craftItemTask.inputs.count, craftingTable, cb))
-        taskQueue.runAll(cb);
     }
 }
